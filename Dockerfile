7924c6322fa7d7c3f59392cf281605b0ffe2bf17
alexander.lovett <alexander.lovett@geant.org>
2020-07-22 13:39:56 +0100

    Added Example of dynamo scan timingout

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM ubuntu:18.04
+FROM ubuntu:20.04
 
 RUN apt-get update
-RUN apt-get install -y git gcc g++ cmake libcurl4-openssl-dev zlib1g zlib1g-dev libssl-dev zip
+RUN DEBIAN_FRONTEND='noninteractive' apt-get install -y git gcc g++ cmake libcurl4-openssl-dev zlib1g zlib1g-dev libssl-dev zip
 
 WORKDIR /usr/src
 RUN git clone https://github.com/awslabs/aws-lambda-cpp.git
@@ -11,9 +11,11 @@ RUN cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF
 RUN make && make install
 
 WORKDIR /usr/src
-RUN mkdir my-src
-RUN echo "#!/bin/bash\ncd my-src/build && cmake .. && make && make aws-lambda-package-hello" > /entry.sh
-RUN chmod +x /entry.sh
+RUN git clone https://github.com/aws/aws-sdk-cpp.git
+RUN mkdir aws-sdk-cpp/build
+WORKDIR /usr/src/aws-sdk-cpp/build
+RUN cmake .. -DBUILD_SHARED_LIBS=OFF -DBUILD_ONLY="dynamodb"
+RUN make && make install
 
 WORKDIR /usr/src
 RUN git clone https://github.com/open-source-parsers/jsoncpp.git
@@ -23,5 +25,11 @@ RUN cmake .. -DBUILD_STATIC_LIBS=ON -DBUILD_SHARED_LIBS=OFF
 RUN make && make install
 
 WORKDIR /usr/src
+RUN mkdir my-src
+RUN echo "#!/bin/bash\ncd my-src/build && cmake .. && make && make aws-lambda-package-hello" > /entry.sh
+RUN chmod +x /entry.sh
+
+RUN gcc -v
+RUN cmake -version
 
 CMD /entry.sh
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,36 @@
+#include <aws/core/Aws.h>
+#include <aws/core/utils/Outcome.h>
+#include <aws/dynamodb/DynamoDBClient.h>
+#include <aws/dynamodb/model/AttributeDefinition.h>
+#include <aws/dynamodb/model/GetItemRequest.h>
+#include <aws/dynamodb/model/ScanRequest.h>
 #include <aws/lambda-runtime/runtime.h>
-#include <json/json.h>
+#include <iostream>
 
 using namespace aws::lambda_runtime;
 
 invocation_response my_handler(invocation_request const& request)
 {
 
-   Json::Value root(Json::objectValue);
-   root["hi"] = Json::Value("there");
+   Aws::Client::ClientConfiguration clientConfig;
+   clientConfig.region = std::getenv("AWS_REGION");
+   Aws::DynamoDB::DynamoDBClient client(clientConfig);
 
-   return invocation_response::success(root.toStyledString(), "application/json");
+   const Aws::DynamoDB::Model::ScanRequest scanRequest = Aws::DynamoDB::Model::ScanRequest().WithTableName("service-catalog.services");
+   std::cout << "about to scan" << std::endl;
+   const Aws::DynamoDB::Model::ScanOutcome &scanResult = client.Scan(scanRequest); // Time out here
+   std::cout << "scan complete" << std::endl;
+
+   return invocation_response::success(scanResult.IsSuccess() ? "true" : "false", "application/json");
 }
 
 int main()
 {
+   Aws::SDKOptions options;
+   Aws::InitAPI(options);
+
    run_handler(my_handler);
+
+   Aws::ShutdownAPI(options);
    return 0;
 }
